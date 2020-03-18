72b3c3a928aaf4a5aacd1173a3b7b9840a4937c3
Henry Villavicencio <henry@MacBook-Pro-de-Henry.local>
2020-03-18 00:39:14 -0500

    fix error *

--- clone_instagram/settings.py
+++ clone_instagram/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '&6-3ghoot=@kwtwoojqxninhs@hv*mrq&z&!d^30ec)gc&7ww('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
