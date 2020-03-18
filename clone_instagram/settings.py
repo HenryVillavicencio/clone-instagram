5b136442e5ec26a7b6939e7aa5b5bd83592f3ee4
Henry Villavicencio <henry@MacBook-Pro-de-Henry.local>
2020-03-17 21:39:06 -0500

    add file requirements

--- clone_instagram/settings.py
+++ clone_instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&6-3ghoot=@kwtwoojqxninhs@hv*mrq&z&!d^30ec)gc&7ww('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
